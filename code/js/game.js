63876a4cdc1af17f0f49639b5a9e0580657f4030
A-Donato <alexis.donato95@gmail.com>
2017-08-23 21:50:15 -0300

    Primera fila de enemigos. La nave se mueve. es posible disparar.No hay colisiones

--- code/js/game.js
+++ code/js/game.js
@@ -1,38 +1,54 @@
-var ship, aliens, score, speed, scoreTextValue, speedTextValuetextStyle_Key, textStyle_Value;
+var ship, spaceKey,aliens, score, speed, scoreTextValue, speedTextValue, textStyle_Key, textStyle_Value, direction, new_direction , cursors;
+var sprite;
+var bullet;
+var bullets;
+var bulletTime = 0;
 
 var Game = {
     preload: function () {
         game.load.image("ship","images/nave.png");
         game.load.image("aliens","images/enemigo.png");
-        game.load.spritesheet("shot","images/shot.png",6,23);
+        game.load.spritesheet("bullet","images/disparo3.png",6,23);
         game.load.spritesheet("space","images/escenario.jpg",353,498);
 
 
     },
     
     create : function () {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
         var space = this.game.add.sprite(0,0,"space");
         var go = space.animations.add("go",[2,4,8,0,5,7,6,9,3,10,1]);
         space.animations.play("go",10, true);
 
         //inicializamos
-        ship = null;
+        ship = game.add.sprite(150,450,"ship");
         aliens = [];
         score = 0;
         speed = 0;
+        direction = "right";
+        new_direction = null;
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.enable(ship,bullets);
+        bullets = game.add.group();
+        bullets.enableBody = true;
+        bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+        bullets.createMultiple(10, 'bullet');
+        bullets.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetBullet, this);
+        bullets.setAll('checkWorldBounds', true);
 
         //controlador de flechitas
         cursors = game.input.keyboard.createCursorKeys();
-
-        //creamos la nave
-        ship = game.add.sprite(60,200,"ship");
-
+        //  Register the keys.
+        this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        //  Stop the following keys from propagating up to the browser
+        game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
 
         //generamos los enemigos
-
-        for(var i = 0; i < 4; i++){
-            aliens[i] = game.add.sprite(20 + 80 * i, 50,"aliens").scale.setTo(0.5,0.5);
+        for(var i = 0; i < 8; i++){
+            aliens[i] = game.add.sprite(20 + 40 * i, 50,"aliens").scale.setTo(0.2,0.2);
         }
 
         textStyle_Key = { font: "bold 14px sans-serif", fill: "#46c0f9", align: "center" };
@@ -47,8 +63,68 @@ var Game = {
     },
 
     update: function () {
+        ship.body.velocity.x = 0;
+        if (cursors.left.isDown)
+        {
+            //  Move to the left
+            ship.body.velocity.x = -150;
+
+            ship.animations.play('left');
+        }
+        else if (cursors.right.isDown)
+        {
+            //  Move to the right
+            ship.body.velocity.x = 150;
+
+            ship.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            stopShip();
+        }
+
+
+        if(ship.x >= 353 || ship.x <=0){
+            ship.animations.stop();
+        }
+       // if (this.spaceKey.isDown)
+        //{
+        //    fireBullet();
+        //}
+
+
+
+        speed = Math.min(10, Math.floor(score/5));
+        // Update speed value on game screen.
+        speedTextValue.text = '' + speed;
+
 
     },
 
+}
+function stopShip(){
+}
+
+function fireBullet (){
+
+    if (game.time.now > bulletTime)
+    {
+        bullet = bullets.getFirstExists(false);
+
+        if (bullet)
+        {
+            bullet.reset(ship.x + 20, ship.y);
+            bullet.body.velocity.y = -300;
+            bulletTime = game.time.now + 250;
+        }
+    }
+
+}
+
+//  Called if the bullet goes out of the screen
+function resetBullet (bullet) {
+
+    bullet.kill();
 
 }
